8b04f2386f9802f51e72df42e782fc75066a8f14
Per Rovegård <per@rovegard.se>
2022-12-07 05:59:28 +0100

    Add utils

--- utils/utils.nim
+++ utils/utils.nim
@@ -2,3 +2,10 @@ iterator slidingWindow*[T](s: seq[T], size: int): (seq[T], int) =
     for i in size..s.len():
         let chunk = s[(i-size)..(i-1)]
         yield (chunk, i)
+
+type
+    Point2D* = object
+        x*, y*: int
+
+proc manhattan*(a, b: Point2D): int =
+    return abs(a.x - b.x) - abs(a.y - b.y)
