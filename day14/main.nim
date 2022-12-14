fc6319d70448b6898245bf18c343801d020ccdae
Per Rovegård <per@rovegard.se>
2022-12-14 06:38:11 +0100

    Day 14, part 2

--- day14/main.nim
+++ day14/main.nim
@@ -3,20 +3,13 @@ import strutils
 import unittest
 import sets
 import options
-import sugar
-import algorithm
 import ../utils/move
-import Tables
 import math
-
-# type
-#     TileKind = enum
-#         tkAir,
-#         tkSand
 
 type
     Path = seq[Coord]
     Cave = HashSet[Coord]
+    OccupiedFn = proc (cave: Cave, c: Coord): bool
 
 proc parseLine(line: string): Path =
     let points = line.split(" -> ")
@@ -56,18 +49,20 @@ proc printCave(cave: Cave) =
         stdout.write('\n')
     flushFile(stdout)
 
-proc nextPos(cave: Cave, c: Coord): Option[Coord] =
+proc nextPos(cave: Cave, c: Coord, isOccupied: OccupiedFn): Option[Coord] =
     let deltas = [(0, 1), (-1, 1), (1, 1)]
     for d in deltas:
         let nxt = Coord(x: c.x + d[0], y: c.y + d[1])
-        if not cave.contains(nxt):
+        if not isOccupied(cave, nxt):
             return some(nxt)
     return none[Coord]()
 
-proc dropOneSand(cave: var Cave, maxY: int): bool =
+proc dropOneSand(cave: var Cave, maxY: int, isOccupied: OccupiedFn): bool =
     var c = Coord(x: 500, y: 0)
+    if isOccupied(cave, c):
+        return false
     while c.y < maxY:
-        let nextOpt = nextPos(cave, c)
+        let nextOpt = nextPos(cave, c, isOccupied)
         if nextOpt.isNone:
             # rest
             cave.incl(c)
@@ -76,20 +71,43 @@ proc dropOneSand(cave: var Cave, maxY: int): bool =
             c = nextOpt.get
     return false
 
-proc part1(file: string): int =
+proc findMaxY(cave: Cave): int = cave.items.toSeq.mapIt(it.y).max()
+
+proc occupiedP1(cave: Cave, c: Coord): bool = cave.contains(c)
+
+proc createOccupiedP2(maxY: int): OccupiedFn =
+    return proc (cave: Cave, c: Coord): bool =
+        if cave.contains(c):
+            return true
+        return c.y == 2 + maxY
+
+proc setup(file: string): (Cave, int) =
     let paths = lines(file).toSeq.map(parseLine)
     var cave = newCave()
 
     populateWithRock(cave, paths)
+    let maxY = findMaxY(cave)
 
-    printCave(cave)
+    return (cave, maxY)
 
-    let maxY = cave.items.toSeq.mapIt(it.y).max()
+
+proc part1(file: string): int =
+    var (cave, maxY) = setup(file)
+
     var dropped = 0
-    while dropOneSand(cave, maxY):
+    while dropOneSand(cave, maxY, occupiedP1):
         dropped += 1
-    
-    printCave(cave)
+
+    return dropped
+
+proc part2(file: string): int =
+    var (cave, maxY) = setup(file)
+
+    let occupiedP2 = createOccupiedP2(maxY)
+    var dropped = 0
+    # Add 3 since we stop at y < max
+    while dropOneSand(cave, maxY + 3, occupiedP2):
+        dropped += 1
 
     return dropped
 
@@ -104,3 +122,7 @@ suite "day 14":
     test "part1":
         check(part1("example") == 24)
         check(part1("input") == 614)
+
+    test "part2":
+        check(part2("example") == 93)
+        check(part2("input") == 26170)
