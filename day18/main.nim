f8cdae146a26418c72642f8492cc73c425e084c1
Per Rovegård <per@rovegard.se>
2022-12-18 08:43:15 +0100

    Simplify day 18

--- day18/main.nim
+++ day18/main.nim
@@ -13,63 +13,26 @@ proc parseLine(line: string): Coord3D =
 
 proc areAdjacent(a, b: Coord3D): bool = manhattan(a, b) == 1
 
-type
-    Cube = ref object
-        pos: Coord3D
-        adjacent: seq[Cube]
+iterator nonDiagonalNeighbors(c: Coord3D): Coord3D =
+    yield Coord3D(x: c.x + 1, y: c.y, z: c.z)
+    yield Coord3D(x: c.x, y: c.y + 1, z: c.z)
+    yield Coord3D(x: c.x, y: c.y, z: c.z + 1)
+    yield Coord3D(x: c.x - 1, y: c.y, z: c.z)
+    yield Coord3D(x: c.x, y: c.y - 1, z: c.z)
+    yield Coord3D(x: c.x, y: c.y, z: c.z - 1)
 
-proc `$`(c: Cube): string =
-    return &"[(x: {c.pos.x}, y: {c.pos.y}, z: {c.pos.z}), adjacent = {c.adjacent.len()}]"
-
-proc `==`(a, b: Cube): bool = a.pos == b.pos
-proc hash(c: Cube): Hash = hash(c.pos)
-
-proc newCube(pos: Coord3D): Cube = Cube(pos: pos, adjacent: newSeq[Cube]())
-
-proc cubeGroup(c: Cube): seq[Cube] =
-    var all = initHashSet[Cube]()
-    var list = @[c]
-    while list.len() > 0:
-        let next = list.pop()
-        all.incl(next)
-        for a in next.adjacent:
-            if all.contains(a) or list.contains(a):
-                continue
-            list.add(a)
-    return all.items.toSeq
-
-proc connectCubes(cubes: var seq[Cube]) =
-    for i in 0..<cubes.len():
-        for j in (i+1)..<cubes.len():
-            var c1 = cubes[i]
-            var c2 = cubes[j]
-            if areAdjacent(c1.pos, c2.pos):
-                c1.adjacent.add(c2)
-                c2.adjacent.add(c1)
-
-iterator droplets(cubes: seq[Cube]): seq[Cube] =
-    var allCubes = cubes.toHashSet()
-    while allCubes.len() > 0:
-        let c = allCubes.pop()
-        let group = cubeGroup(c)
-
-        for c2 in group:
-            allCubes.excl(c2)
-
-        yield group
-
-proc countArea(cubes: seq[Cube]): int =
-    var allCubes = cubes
-    connectCubes(allCubes)
+proc countArea(cubes: seq[Coord3D]): int =
+    let cubePositions = cubes.toHashSet
 
     var area = 0
-    for droplet in droplets(allCubes):
-        area += droplet.mapIt(6 - it.adjacent.len()).sum()
+    for c in cubes:
+        let neighborCount = nonDiagonalNeighbors(c).toSeq.filterIt(cubePositions.contains(it)).len()
+        area += 6 - neighborCount
 
     return area
 
 proc part1(file: string): int =
-    var cubes = lines(file).toSeq.map(parseLine).map(newCube)
+    var cubes = lines(file).toSeq.map(parseLine)
     return countArea(cubes)
 
 suite "day 18":
@@ -79,14 +42,14 @@ suite "day 18":
 
     test "countArea":
         check(countArea(@[
-           newCube(Coord3D(x: 1, y: 1, z: 1)),
-           newCube(Coord3D(x: 2, y: 1, z: 1))
+           Coord3D(x: 1, y: 1, z: 1),
+           Coord3D(x: 2, y: 1, z: 1)
         ]) == 10)
 
         check(countArea(@[
-            newCube(Coord3D(x: 1, y: 1, z: 1)),
-            newCube(Coord3D(x: 2, y: 1, z: 1)),
-            newCube(Coord3D(x: 3, y: 1, z: 1))
+            Coord3D(x: 1, y: 1, z: 1),
+            Coord3D(x: 2, y: 1, z: 1),
+            Coord3D(x: 3, y: 1, z: 1)
         ]) == 14)
 
 
